d1195505efc338429bc368072b9d72ea34dc6638
Cássio Nunes <cassioczo@hotmail.com>
2026-05-20 11:17:58 -0300

    fix: remove nodemailer from server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,8 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import nodemailer from "nodemailer";
+
+import contactRoutes from "./routes/contactRoutes.js";
 
 dotenv.config();
 
@@ -12,50 +11,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
+app.use("/", contactRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API do portfólio rodando 🚀");
 });
 
-app.post("/send-email", async (req, res) => {
-  try {
-    const { name, email, subject, message } = req.body;
+const PORT = process.env.PORT || 3000;
 
-    await transporter.sendMail({
-      from: process.env.EMAIL_USER,
-      to: process.env.EMAIL_USER,
-      subject: `Portfólio: ${subject}`,
-      html: `
-        <h2>Novo contato do portfólio</h2>
-
-        <p><strong>Nome:</strong> ${name}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Assunto:</strong> ${subject}</p>
-
-        <p><strong>Mensagem:</strong></p>
-        <p>${message}</p>
-      `,
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "Email enviado com sucesso!",
-    });
-  } catch (error) {
-    console.log(error);
-
-    res.status(500).json({
-      success: false,
-      message: "Erro ao enviar email",
-    });
-  }
-});
-
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });

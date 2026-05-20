c6204685f5b74822a4c56d29a3bc3a641a0870a2
Cássio Nunes <cassioczo@hotmail.com>
2026-05-20 09:50:21 -0300

    fix: update gmail smtp port

--- backend/src/routes/contactRoutes.js
+++ backend/src/routes/contactRoutes.js
@@ -9,14 +9,11 @@ router.post("/send-email", async (req, res) => {
 
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
-      },
-      tls: {
-        rejectUnauthorized: false,
       },
     });
 
